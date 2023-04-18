8a753256a907a54f04e882bf8fdbf1e295cb7f04
angsoenW <91903037+angsoenW@users.noreply.github.com>
2023-04-17 23:55:12 -0700

    Update app.java

--- app.java
+++ app.java
@@ -1,47 +1,50 @@
-import java.net.*;
+
 import java.io.*;
-
+import java.net.*;
+// java TCPClient host port
 public class app {
-    public static void main(String[] args) {
-        try {
-            // while (!System.console().readLine().equals("stop")){
-            //     System.out.println("What is the host address?");
-            //     String name = System.console().readLine();
-            //     System.out.println("What is the port number?");
-            //     String port = System.console().readLine();
-            //     Socket socket = new Socket(name, Integer.valueOf(port));
-            //     InputStream in = socket.getInputStream();
-            //     int readChar = 0;
+public static void main(String... args) {
+String host = args[0];
+int port = Integer.valueOf(args[1]);
+try (Socket sock = new Socket(host, port)) {
+OutputStream out = sock.getOutputStream();
+String get = "GET / HTTP/1.0\n\n";
+out.write(get.getBytes());
+InputStream in = sock.getInputStream();
+int readChar = 0;
+while ((readChar = in.read()) != -1) {
+System.out.write(readChar);
+}
+}
+catch (IOException ex) {
+ex.printStackTrace();
+}
+}
+}
 
-            //     while((readChar = in.read()) != -1) {
-            //         System.out.write(readChar);
-            //     }
-
-            //     in.close();
-            //     socket.close();
-            // }
+        
             
 
-            for (int i = 0; i < args.length; i+=2){
-                Socket socket = new Socket(args[i], Integer.valueOf(args[i+1]));
-                InputStream in = socket.getInputStream();
-                int readChar = 0;
-                while((readChar = in.read()) != -1) {
-                    System.out.write(readChar);
-                }
+        //     for (int i = 0; i < args.length; i+=2){
+        //         Socket socket = new Socket(args[i], Integer.valueOf(args[i+1]));
+        //         InputStream in = socket.getInputStream();
+        //         int readChar = 0;
+        //         while((readChar = in.read()) != -1) {
+        //             System.out.write(readChar);
+        //         }
 
-                in.close();
-                socket.close();
-            }
-        }
+        //         in.close();
+        //         socket.close();
+        //     }
+        // }
 
-        catch (Exception e) {
-            System.out.println(e);
-        }
+// //         catch (Exception e) {
+// //             System.out.println(e);
+// //         }
        
         
 
-    }
+// //     }
     
-}
+// // }
 
